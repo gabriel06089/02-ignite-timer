fa63b0b844c62fc91c42b5af64ede76afe6360bd
Gabriel Alves <gabrielemailpessoal02@gmail.com>
2023-04-19 16:00:01 -0300

    Configurando temas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,24 +1,32 @@
-import styled, {css} from 'styled-components';
-
+import styled, { css } from 'styled-components';
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
-interface ButtonContainerProps{
-    variant: ButtonVariant;
+interface ButtonContainerProps {
+  variant: ButtonVariant;
 }
-const ButtonVariants={
-    primary: 'purple',
-    secondary: 'orange',
-    danger:'red',
-    success:'green',
-
-}
+const ButtonVariants = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
+};
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
- width: 100px;
- height: 40px;
+  width: 100px;
+  height: 40px;
 
- ${props => {
-    return css`background-color: ${ButtonVariants[ props.variant]}`
- }}
-`
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
+
+  /*
+  * ${(props) => {
+    return css`
+      background-color: ${ButtonVariants[props.variant]};
+    `;
+  }}
+  */
+`;
